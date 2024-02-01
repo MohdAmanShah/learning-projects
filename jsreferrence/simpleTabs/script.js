d6002c740bd14ee04fd671835312e42aaa806546
Mohd Aman Shah <reach.mohdamanshah@gmail.com>
2024-02-01 08:01:33 +0530

    added wheel event in tab project

--- jsreferrence/simpleTabs/script.js
+++ jsreferrence/simpleTabs/script.js
@@ -1,5 +1,8 @@
 const Links = document.querySelectorAll("[data-tab-panel] > li > a");
 const Tabs = document.querySelectorAll("[data-tab-content]");
+let Title = ["Mohd Aman Shah - ", Tabs.item(0).id];
+let TabIndex = 1;
+document.title = Title.join("");
 Links.forEach((element) => {
   element.addEventListener("click", LinkEventHandler);
 });
@@ -11,6 +14,18 @@ function LinkEventHandler(element) {
     ele.classList.remove("active");
   });
   element.target.classList.add("active");
-  document.querySelector(element.target.dataset.tabTarget).classList.add("active");
+  document
+    .querySelector(element.target.dataset.tabTarget)
+    .classList.add("active");
+  Title[1] = element.target.dataset.tabTarget.substr(1);
+  document.title = Title.join("");
 }
-//change
+
+document.addEventListener("wheel", function (event) {
+  if (event.deltaY > 0) {
+    TabIndex = Math.min(TabIndex + 1, Links.length - 1);
+  } else {
+    TabIndex = Math.max(TabIndex - 1, 0);
+  }
+  Links.item(TabIndex).click();
+});
